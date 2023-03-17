0746209172066158eeab834fa4f6f1c817487ac5
rlagns1234 <126846654+rlagns1234@users.noreply.github.com>
2023-03-17 14:05:18 +0900

    Add files via upload

---
+++ hw2.py
@@ -0,0 +1,10 @@
+def exchange(amount):
+    coin = [500, 100, 50, 10]
+    for i in coin:
+        print(i, '원 동전의 개수:', amount//i)
+        amount%=i
+
+def get_integer():
+    return int(input('동전으로 교환하고자 하는 금액은? '))
+
+exchange(get_integer())
